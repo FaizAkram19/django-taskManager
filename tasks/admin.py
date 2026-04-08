dbfcaa45b518a4ea42d099b7112d95ed0ca22e12
Faiz Akram <172576315+FaizAkram19@users.noreply.github.com>
2026-04-08 17:40:47 +0000

    Customized the admin site and registered the models

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Priority, Task
+
+class TaskAdmin(admin.ModelAdmin):
+    fieldsets=[
+        ("General Information", {"fields":["title", "description"]}),
+        ("Date Information", {"fields":["due_date"]})
+    ]
+    list_display=["title", "pub_date", "due_date","is_completed"]
+    list_filter=["priorityLevel"]
+    search_fields=["title"]
+
+admin.site.register(Task, TaskAdmin) # this takse only the model and optionally the admin class
+admin.site.register(Priority)
